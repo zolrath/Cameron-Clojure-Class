a3688ed030ec70a284ec6a5c295bb925547a8fd9
Matt Furden <zolrath@gmail.com>
2011-10-12 04:04:25 -0700

    Added ability to assign questions to categories and only ask them if category was performed.

--- src/cameron/question.clj
+++ src/cameron/question.clj
@@ -4,29 +4,63 @@
 
 (defn add-headers? [filename headers]
   "Checks to see if file exists, if not, add each element of headers vector."
-  (if (false? (.exists (io/file (str filename ".csv"))))
-    (with-open [file (io/writer (io/file (str filename ".csv")))]
-      (spit file (str/join "," (conj headers \newline))))))
+  (let [filecsv (io/file (str filename ".csv"))]
+        (if (false? (.exists filecsv))
+        (with-open [file (io/writer filecsv)]
+          (spit file (str/join "," (conj headers \newline)))))))
 
 (defn add-to-file [filename data]
   "Appends all elements in a given vector of data to a CSV file of given name."
-  (with-open [file (io/writer (io/file (str filename ".csv"))
-                              :append true)]
-    (spit file (str/join "," (conj data \newline)))))
+  (let [filecsv (io/file (str filename ".csv"))]
+    (with-open [file (io/writer filecsv :append true)]
+      (spit file (str/join "," (conj data \newline))))))
 
-(defn get-input [prompt]
-  "Prints out prompt and returns user input."
-  (println prompt)
-  (read-line))
+(defn get-input [category prompt]
+  "Bind result of ask-category to a var and use it as the category field if the
+   question must be asked regardless of category answer, set category to true."
+  (if category
+    (do (println prompt)
+        (read-line))))
+
+(defn y-or-n []
+  "Return true on y, false on n. On any other answer, prompt for y or n."
+  (let [answer (read-line)]
+    (if (= answer "y") true
+        (if (= answer "n") false
+            (do (println "Please answer y or n") (recur))))))
+
+(defn ask-category [prompt]
+  "Asks if you participated in given category. Bind result to var for use in get-input."
+  (println (str prompt " (y/n)"))
+  (y-or-n))
 
 (defn prompt []
   "Add data to CSV file as long as user answers y or yes to Add another?"
-  (add-headers? "data" ["Name" "Age" "Sex" "Has Monkey?"])
+  (add-headers? "data" ["Date" "Mood" "Miles Driven"
+                        "Books Name" "Number of Pages"
+                        "Movie Seen" "Home/Theater" "Movie Seen Alone?"
+                        "Board Game Name" "Time Spent Playing"])
   (loop []
-    (let [name   (get-input "Who we talkin' about here? ")
-          age    (get-input "How old is the bum? ")
-          sex    (get-input "Are they sexy? Errr... Male or Female?")
-          monkey (get-input "Do they have a monkey? That funky monkey. Brass Monkey. ")]
-      (add-to-file "data" [name age sex monkey])
-      (if (= (get-input "Add another? (y or yes)") (or "y" "yes" "Y" "Yes"))
+    (let [date       (get-input true "What day are we talking about? (MM/DD/YYYY)")
+          mood       (get-input true "How are you feeling today? (Scale of 1-10)")
+          miles      (get-input true "How many miles did you drive today?")
+
+          reading    (ask-category "Did you read any books today?")
+          bookname   (get-input reading "What was the books title?")
+          pagenum    (get-input reading "How many pages did you read?")
+
+          movies     (ask-category "Did you watch a movie today?")
+          movietitle (get-input movies "What movie was it?")
+          movieloc   (get-input movies "Did you see it at Home or at the Theater?")
+          moviealone (get-input movies "Did you watch it alone? (y/n)")
+
+          boardgames (ask-category "Did you play any board games today?")
+          bgamename  (get-input boardgames "What boardgame did you play?")
+          bgametime  (get-input boardgames "How long did it take to play? (in minutes)")]
+      (add-to-file "data" [date mood miles
+                           bookname pagenum
+                           movietitle movieloc moviealone
+                           bgamename bgametime])
+      (println "Add another? (y or n)")
+      (if (y-or-n)
         (recur)))))
