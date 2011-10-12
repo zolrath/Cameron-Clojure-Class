e6f5b6a9c499027a91e9ce16b0192ad3214ef07b
Matt Furden <zolrath@gmail.com>
2011-10-12 05:55:31 -0700

    Added ability to assign validation to questions.

--- src/cameron/question.clj
+++ src/cameron/question.clj
@@ -15,12 +15,12 @@
     (with-open [file (io/writer filecsv :append true)]
       (spit file (str/join "," (conj data \newline))))))
 
-(defn get-input [category prompt]
-  "Bind result of ask-category to a var and use it as the category field if the
-   question must be asked regardless of category answer, set category to true."
-  (if category
-    (do (println prompt)
-        (read-line))))
+(defn this-or-that [[this that]]
+  "Takes a vector of two choices and will only allow accept either choice as the answer."
+  (let [answer (read-line)]
+    (if (= (.toLowerCase answer) (.toLowerCase this)) this
+        (if (= (.toLowerCase answer) (.toLowerCase that)) that
+            (do (println (str "Please answer " this " or " that)) (recur [this that]))))))
 
 (defn y-or-n []
   "Return true on y, false on n. On any other answer, prompt for y or n."
@@ -29,10 +29,46 @@
         (if (= answer "n") false
             (do (println "Please answer y or n") (recur))))))
 
+(defn input-scale [[min max]]
+  "Takes a vector of minimum and maximum values of input."
+  (let [answer (Integer/parseInt (read-line))]
+    (if (and (>= answer min) (<= answer max))
+      answer
+      (do (println (str "Please answer between " min " and " max))
+          (recur [min max])))))
+
+(defn input-date []
+  "Requires that the date be entered in a valid and standardized format."
+  (let [answer (read-line)
+        date   (re-find #"(\d{2})/(\d{2})/\d{4}" answer)
+        month  (Integer/parseInt (or (nth date 1) "0"))
+        day    (Integer/parseInt (or (nth date 2) "0"))]
+    (if (and (> month 0) (< month 13) (> day 0) (< day 32))
+      answer
+      (do (println "Please input the date in the format of MM/DD/YYYY (e.g. 01/25/1986)")
+          (recur)))))
+
 (defn ask-category [prompt]
   "Asks if you participated in given category. Bind result to var for use in get-input."
   (println (str prompt " (y/n)"))
   (y-or-n))
+
+(defn get-input [category prompt & validation]
+  "Bind result of ask-category to a var and use it as the category field if the
+   question must be asked regardless of category answer, set category to true.
+   Can be passed optional keyword to determine input type.
+   :y-or-n makes the question only accept y or n.
+   :this-or-that only accept varibles in given vector.
+   :scale only accept answers between 1 and 10
+   :date only accepts numbers in format of MM/DD/YYYY"
+  (if category
+    (do (println prompt)
+        (case (first validation)
+          :y-or-n (y-or-n)
+          :this-or-that (this-or-that (first (rest validation)))
+          :scale (input-scale (first (rest validation)))
+          :date (input-date)
+          nil (read-line)))))
 
 (defn prompt []
   "Add data to CSV file as long as user answers y or yes to Add another?"
@@ -41,8 +77,8 @@
                         "Movie Seen" "Home/Theater" "Movie Seen Alone?"
                         "Board Game Name" "Time Spent Playing"])
   (loop []
-    (let [date       (get-input true "What day are we talking about? (MM/DD/YYYY)")
-          mood       (get-input true "How are you feeling today? (Scale of 1-10)")
+    (let [date       (get-input true "What day are we talking about? (MM/DD/YYYY)" :date)
+          mood       (get-input true "How are you feeling today? (Scale of 1-10)" :scale [1 10])
           miles      (get-input true "How many miles did you drive today?")
 
           reading    (ask-category "Did you read any books today?")
@@ -51,8 +87,8 @@
 
           movies     (ask-category "Did you watch a movie today?")
           movietitle (get-input movies "What movie was it?")
-          movieloc   (get-input movies "Did you see it at Home or at the Theater?")
-          moviealone (get-input movies "Did you watch it alone? (y/n)")
+          movieloc   (get-input movies "Did you see it at Home or at the Theater?" :this-or-that ["Home" "Theater"])
+          moviealone (get-input movies "Did you watch it alone? (y/n)" :y-or-n)
 
           boardgames (ask-category "Did you play any board games today?")
           bgamename  (get-input boardgames "What boardgame did you play?")
